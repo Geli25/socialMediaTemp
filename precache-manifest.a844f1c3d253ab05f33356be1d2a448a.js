532be0c9c35131b7abfa57cd1ca249bfdbbd4dce
Angelina <h1255513@gmail.com>
2019-07-13 22:34:02 -0400

    Updates

---
+++ precache-manifest.a844f1c3d253ab05f33356be1d2a448a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f7685bee1ef5b6a5fac1e7d96b94e0",
+    "url": "/socialMediaTemp/index.html"
+  },
+  {
+    "revision": "450bcf717b8a491d06cb",
+    "url": "/socialMediaTemp/static/css/main.882945fa.chunk.css"
+  },
+  {
+    "revision": "b5f95ac3c43e70f32ae0",
+    "url": "/socialMediaTemp/static/js/2.21ebc7e4.chunk.js"
+  },
+  {
+    "revision": "450bcf717b8a491d06cb",
+    "url": "/socialMediaTemp/static/js/main.7e30d37e.chunk.js"
+  },
+  {
+    "revision": "995564c09cd2e30e789e",
+    "url": "/socialMediaTemp/static/js/runtime~main.a856e301.js"
+  }
+]);
